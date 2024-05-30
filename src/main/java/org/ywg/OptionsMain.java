ca3636ff882399c4e9d0e78d1a6ceee964b613c2
ywg <281003252@qq.com>
2024-05-30 15:53:47 +0800

    Initial commit

---
+++ src/main/java/org/ywg/OptionsMain.java
@@ -0,0 +1,11 @@
+package org.ywg;
+
+import com.example.options.OptionMessageOther;
+
+public class OptionsMain {
+
+
+    public static void main(String[] args) {
+        OptionMessageOther other = OptionMessageOther.newBuilder().build();
+    }
+}
